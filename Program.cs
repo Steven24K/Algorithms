8d5a85fc0aeb2cd57f3f77313a49f112172afc9d
Steven <s.koerts2@gmail.com>
2025-02-14 13:15:20 +0100

    Let's get started

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArrayExtensions;
+
+int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int indexFound = numbers._Find(50);
+
+
+int[] arr1 = new int[] { 1, 2, 3 };
+int[] arr2 = new int[] { 4, 5 };
+var double_arr = arr1._Zip(arr2);
+
+Console.WriteLine(double_arr._Print2DArray());
+
+Console.WriteLine($"Found value at index: {indexFound}");
+
+Console.WriteLine("Hello, World!");
