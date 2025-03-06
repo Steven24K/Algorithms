43ab09882273fd267c1a71e8cad7233087712613
Steven <s.koerts2@gmail.com>
2025-03-06 15:56:24 +0100

    Boilerplate For Test

---
+++ Test/TestSorting.cs
@@ -0,0 +1,22 @@
+namespace Test;
+
+public class TestSorting
+{
+    [Fact]
+    public void TestBubblieSort()
+    {
+    
+    }
+
+    [Fact]
+    public void TestInsertionSort()
+    {
+
+    }
+
+    [Fact]
+    public void TestMergeSort()
+    {
+
+    }
+}
